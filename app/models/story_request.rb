4e0a5fcde69a06713a2e63d52b80631a6175384e
Nícolas Iensen <nicolas.iensen@gmail.com>
2023-05-19 21:08:33 +0000

    Enqueue job upon story request creation

--- app/models/story_request.rb
+++ app/models/story_request.rb
@@ -1,3 +1,9 @@
 class StoryRequest < ApplicationRecord
   has_one :story
+
+  after_commit :enqueue_story_creation, on: :create
+
+  def enqueue_story_creation
+    StoryCreationJob.perform_later(self)
+  end
 end
