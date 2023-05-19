b0ad431b1adcb9496db6cf329b2b5fd91ef112be
Nícolas Iensen <nicolas.iensen@gmail.com>
2023-05-19 21:46:20 +0000

    Remove obsolete actions in the stories controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources :story_requests, only: [:new, :show, :create]
-  resources :stories, only: [:new, :show, :create]
+  resources :stories, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   if Rails.env.development?
